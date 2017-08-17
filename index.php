e3dee1b8d573447f5f709740a7a1410cd4a29cfe
sollar777 <sollar777@gmail.com>
2017-08-17 00:06:00 -0300

    criado função para excluir usuario

--- index.php
+++ index.php
@@ -84,13 +84,26 @@ $app->get('/admin/users/:iduser/delete', function($iduser){
 
 	User::verifyLogin();
 
+	$user = new User();
+	$user->get((int)$iduser);
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
 });
 
 $app->get('/admin/users/:iduser', function($iduser){
 	
 	User::verifyLogin();
+
+	$user = new User();
+	$user->get((int)$iduser);
+
 	$page = new PageAdmin();
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+			"user"=>$user->getValues()
+		));
 
 });
 
@@ -111,6 +124,15 @@ $app->post('/admin/users/create', function(){
 $app->post('/admin/users/:iduser', function($iduser){
 
 	User::verifyLogin();
+
+	$user = new User();
+	$user->get((int)$iduser);
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->setData($_POST);
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
 
 });
 
--- views-cache/users-update.a3060965c628cb9f7496c4d4374d4377.rtpl.php
+++ views-cache/users-update.a3060965c628cb9f7496c4d4374d4377.rtpl.php
@@ -18,27 +18,27 @@
         </div>
         <!-- /.box-header -->
         <!-- form start -->
-        <form role="form" method="post" action="/admin/users">
+        <form role="form" method="post" action="/admin/users/<?php echo htmlspecialchars( $user["iduser"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
           <div class="box-body">
             <div class="form-group">
               <label for="desperson">Nome</label>
-              <input type="text" class="form-control" id="desperson" name="desperson" placeholder="Digite o nome">
+              <input type="text" class="form-control" id="desperson" name="desperson" placeholder="Digite o nome" value="<?php echo htmlspecialchars( $user["desperson"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
             </div>
             <div class="form-group">
               <label for="deslogin">Login</label>
-              <input type="text" class="form-control" id="deslogin" name="deslogin" placeholder="Digite o login" >
+              <input type="text" class="form-control" id="deslogin" name="deslogin" placeholder="Digite o login" value="<?php echo htmlspecialchars( $user["deslogin"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
             </div>
             <div class="form-group">
               <label for="nrphone">Telefone</label>
-              <input type="tel" class="form-control" id="nrphone" name="nrphone" placeholder="Digite o telefone" >
+              <input type="tel" class="form-control" id="nrphone" name="nrphone" placeholder="Digite o telefone" value="<?php echo htmlspecialchars( $user["nrphone"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
             </div>
             <div class="form-group">
               <label for="desemail">E-mail</label>
-              <input type="email" class="form-control" id="desemail" name="desemail" placeholder="Digite o e-mail">
+              <input type="email" class="form-control" id="desemail" name="desemail" placeholder="Digite o e-mail" value="<?php echo htmlspecialchars( $user["desemail"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
             </div>
             <div class="checkbox">
               <label>
-                <input type="checkbox" name="inadmin" value="1"
+                <input type="checkbox" name="inadmin" value=1 <?php if( $user["inadmin"] == 1 ){ ?>checked<?php } ?>> Acesso de administrador
               </label>
             </div>
           </div>
